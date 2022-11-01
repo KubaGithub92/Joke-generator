927d1e4a2b8e7b6f39db7dc6cd0e54a48b3d7088
KubaGithub92 <jakub.rosol@email.cz>
2022-11-01 16:09:47 +0100

    joke generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [joke, setJoke] = useState(null);
+
+  const loadData = async () => {
+    const response = await fetch("https://v2.jokeapi.dev/joke/Any");
+    const data = await response.json();
+    setJoke(data);
+    console.log(data);
+    console.log(data.type);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random joke generator</h1>
+      <button onClick={loadData}>Generate Joke</button>
+      {joke && joke.type === "single" ? (
+        <>
+          <div>{joke.joke}</div>
+        </>
+      ) : (
+        ""
+      )}
+
+      {joke && joke.type === "twopart" ? (
+        <>
+          <div>{joke.setup}</div>
+          <div>{joke.delivery}</div>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
-
 export default App;
